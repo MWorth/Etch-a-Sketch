62356d07fe537887b4f63e96e9b0bc4b0e23e20a
MWorth <MattWorth9@protonmail.com>
2021-11-06 23:31:58 +0000

    Added code for HTML, CSS and Javascript to create an Etch-a-Sketch webpage. Fully working for a set 16 x 16 grid with a reset button. In the middle of converting it to allow the user to pick the size of the grid.

---
+++ script.js
@@ -0,0 +1,38 @@
+var colour = "black";
+var size = 16;
+createBoard(size);
+const divs = document.querySelector('#container');
+divs.addEventListener('mouseover', (div) => {
+    mouseOver(div.target.id);
+});
+
+const button = document.querySelector('#reset');
+button.addEventListener('click', () => {
+    resetBoard();
+});
+
+function createBoard(boardSize) {
+    var elem0 = document.querySelector('#container');
+
+    for (let i = 0; i < (boardSize * boardSize); i++) {
+        let div = document.createElement("div");
+        div.id = "div-" + i;
+        div.className = "div-";
+//        div.innerHTML = i;
+        elem0.appendChild(div);
+    }
+}
+
+function mouseOver(ID) {
+    document.getElementById(ID).style.backgroundColor = colour;
+  }
+
+function resetBoard() {
+    var divs = document.querySelectorAll(".div-");
+    var length = divs.length;
+    for (let i = 0; i < length; i++) {
+        divs[i].style.removeProperty("background-color");
+    }
+    size = window.prompt("What size grid would you like?", "16")
+    createBoard(size);
+}
